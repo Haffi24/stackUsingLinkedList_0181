08f11f2fe748684091633e1a170d33ac9d544aee
Haffi24 <haffisaefulloh@gmail.com>
2025-05-22 09:59:01 +0700

    menambahkan fungsi push

--- stackUsingLinkelist.cpp
+++ stackUsingLinkelist.cpp
@@ -22,6 +22,12 @@ public:
 stack(){
  top = NULL;
 }
-
+int push (int value) {
+Node* newNode = new Node();
+newNode->data = value;
+newNode->next = top;
+top = newNode;
+cout << "Push value: " << value << endl;
+}
 
 };
