0d75de4dcdbfdb83e3972ef48a38b98285331f76
Haffi24 <haffisaefulloh@gmail.com>
2025-05-22 09:46:01 +0700

    menambahkan deklarasi class node

--- stackUsingLinkelist.cpp
+++ stackUsingLinkelist.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
 
 using namespace std;
+
+//node class representating a single node in the linked list
+class node{
+    public:
+    int data;
+    node* next;
+
+    node(){
+        
+    }
+}
